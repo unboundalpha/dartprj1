9b493cd9aab48d257cc0be87fd5b89f0a92b6f70
unboundalpha <benabrahamboby31@gmail.com>
2023-10-26 19:43:58 +0530

    first commit

---
+++ bin/dartprj1.dart
@@ -0,0 +1,5 @@
+import 'package:dartprj1/dartprj1.dart' as dartprj1;
+
+void main() {
+  print("hey fools");
+}
---
+++ test/dartprj1_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartprj1/dartprj1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
